7c2c2a1951c8713bf8f380cc00623f032175eed8
unknown <kiet>
2023-03-14 09:20:56 +0700

    up code

--- lab 2/bai1.js
+++ lab 2/bai1.js
@@ -1,16 +1,16 @@
-//enter a, b, c
-let a1 = prompt("Nhap a:");
-let b1 = prompt("Nhap b:");
-let c1 = prompt("Nhap c:");
-
-let a = parseInt(a1);
-let b = parseInt(b1);
-let c = parseInt(c1);
-
- if(a == 0){
- if(b == 0){
- if(c == 0){
-    document.write("phuong trinh vo so nghiem");
+//nhập a,b,c vào
+let a = prompt("Nhập a");
+let a1 = parseInt(a);
+let b = prompt("Nhập b");
+let b1 = parseInt(b);
+let c = prompt("Nhập c");
+let c1 = parseInt(c);
+// sử dụng câu lệnh đk 
+if (a == 0) {
+if (b == 0) {
+if (c == 0) {
+  //in ra màn hình
+  document.write("phuong trinh vo so nghiem");
 } else {
   document.write("phuong trinh vo nghiem");
 }
@@ -28,8 +28,8 @@ let x = -b / (2 * a);
 let x1 = (-b + Math.sqrt(delta)) / (2 * a);
 let x2 = (-b - Math.sqrt(delta)) / (2 * a);
 
-document.write
-("phuong trinh co 2 nghiem la " + x1.toFixed(2) + " " + x2.toFixed(2))
-
- };
- }
+document.write(
+  "phuong trinh co 2 nghiem la " + x1.toFixed(2) + " " + x2.toFixed(2)
+);
+}
+}
