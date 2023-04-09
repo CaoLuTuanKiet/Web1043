29c7372abfee13d0c03a55ebf3d164d3e5e29c93
unknown <kiet>
2023-04-09 22:46:10 +0700

    up code

---
+++ Assigment/Ass.js
@@ -0,0 +1,57 @@
+const next = document.querySelector('.next')
+const prev = document.querySelector('.prev')
+const comment = document.querySelector('#list-comment')
+const commentItem = document.querySelectorAll('#list-comment .item')
+var translateY = 0
+var count = commentItem.length
+
+next.addEventListener('click', function (event) {
+  event.preventDefault()
+  if (count == 1) {
+    // Xem hết bình luận
+    return false
+  }
+  translateY += -400
+  comment.style.transform = `translateY(${translateY}px)`
+  count--
+})
+
+prev.addEventListener('click', function (event) {
+  event.preventDefault()
+  if (count == 3) {
+    // Xem hết bình luận
+    return false
+  }
+  translateY += 400
+  comment.style.transform = `translateY(${translateY}px)`
+  count++
+})
+// Banner
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Dùng để điều khiển 
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Điều khiển ảnh
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
